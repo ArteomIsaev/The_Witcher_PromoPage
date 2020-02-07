6b2ece7a1890b2a10a4a092b9d959db129726505
ArteomIsaev <arteom.isaev@icloud.com>
2020-02-07 22:58:34 +0300

    adaptability added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 2,
+    spaceBetween: 2,
     loop: true,
     navigation: {
         nextEl: '.arrow',
       },
+    breakpoints: {
+      540: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      }
+    }
 });
+
+let menuBtn = document.querySelector('.menu-btn')
+let menu = document.querySelector('.header')
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('header-active')
+  menuBtn.classList.toggle('menu-btn-active')
+})
